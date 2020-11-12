3e8245b5a1d63c2a8964cb7767f6c1a934448161
Anel <a.yerzhanova@devir.kz>
2020-11-12 12:18:31 +0600

    f

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const PORT = 3000; // todo change to config file
+const path = require('path');
+
+// app.get('/', (req, res) => {
+// 	// res.send('<h1>Hello</h1>');
+// 	// res.send('<input>Hello</input>');
+// 	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
+
+
+// Set static folder
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+const members = require('./members');
+const logger = require('./middleware/logger');
+//Init middleware
+app.use(logger);
+//Gets all members
+app.get('/api/members', (req, res) => {
+	res.json(members);
+});
+
+//Get single member
+app.get('/api/members/:id', (req, res) => {
+	// res.send(req.params.id);
+	const found = members.some(member => member.id === parseInt(req.params.id));
+	if (found) {
+		// res.json(members.filter(member => member.id === parseInt((req.params.id)));
+	} else {
+		res.status(400).json({msg: `message member not found with the id ${req.params.id}`});
+	}
+})
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
---
+++ members.js
@@ -0,0 +1,16 @@
+const members = [
+	{
+		id: 1,
+		name: 'John Snow',
+		email: 'j.snow@vesteros.com',
+		status: 'active'
+	},
+	{
+		id: 2,
+		name: 'Brann Stark',
+		email: 'b.stark@winterfell.com',
+		status: 'inactive'
+	}
+];
+
+module.exports = members;
